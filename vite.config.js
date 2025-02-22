07109dcd78479737a1778447462a017960ecda04
akizyfee <87452790+akizyfee@users.noreply.github.com>
2025-02-22 15:59:43 +0800

    git page

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mahouData ",
+  base: "/mahouData/",
   plugins: [
     vue(),
     vueJsx(),
